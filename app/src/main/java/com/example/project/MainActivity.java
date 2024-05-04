b81a90e10e02f5d7aaafaa61cb531802b5f2123b
a23dilel <a23dilel@student.his.se>
2024-05-04 16:12:36 +0200

    Updated button ID from "button" to "openSecondActivityButton" and replaced functionality of myPreferenceEditor with intent in onClick method in MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,17 +29,12 @@ public class MainActivity extends AppCompatActivity {
         // trying to find preference data and if data was found then print value on textView otherwise print "No preference found".
         prefTextRef.setText(myPreferenceRef.getString("data1", "No preference found."));
 
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.openSecondActivityButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Grab edit view id
-                EditText editText = findViewById(R.id.editText);
-
-                // add string value and commit.
-                myPreferenceEditor.putString("data1", editText.getText().toString());
-                myPreferenceEditor.commit();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
