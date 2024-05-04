7b2f527196363fbe50ce082e26866a884ff7636b
a23dilel <a23dilel@student.his.se>
2024-05-04 17:36:17 +0200

    Implemented functionality for SharedPreferences, SharedPreferences.Editor and button in SecondActivity.java

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,20 +1,38 @@
 package com.example.project;
 
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        SharedPreferences myPreferenceRef = getSharedPreferences("preference", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
+        Button button = findViewById(R.id.save_preference);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Grab edit view id
+                EditText editText = findViewById(R.id.editText);
+
+                // add string value and commit.
+                myPreferenceEditor.putString("data1", editText.getText().toString());
+                myPreferenceEditor.commit();
+                finish();
+            }
+        });
     }
 
 }
